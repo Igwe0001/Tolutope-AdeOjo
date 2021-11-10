e0a8b488e889ba06b4235df7a22110cee8a6a47b
Igwe0001 <nwobodoemmanuel479@gmail.com>
2021-11-10 13:10:39 +0100

    New Additions

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('nav');
-    header.classList.toggle('sticky', window.scrollY > 0)
-  })
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
 
-  const ham = document.querySelector('.hamburger');
-  const navLinks = document.querySelector('.nav-links');
-  const subList = document.querySelectorAll('.nav-links > li')
-
+const ham = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const subList = document.querySelectorAll(".tweak > li");
 
 //   const nav = document.querySelector('nav');
 //   const belowNav = nav.nextElementSibling;
 //   belowNav.style.marginTop = nav.clientHeight + "px";
 
-  
-ham.addEventListener("click", function() {
-    ham.classList.toggle("kin");
-    navLinks.classList.toggle("view");
-   // navList.classList.remove('show-list');
-  })
+ham.addEventListener("click", function () {
+  ham.classList.toggle("kin");
+  navLinks.classList.toggle("view");
+  // navList.classList.remove('show-list');
+});
+
+subList.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("view");
+    ham.classList.remove('kin');
+  });
+});
+
+  let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
