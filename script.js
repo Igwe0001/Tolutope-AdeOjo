524123200b21f8601800426fa1bea8a32cd8460c
Igwe0001 <nwobodoemmanuel479@gmail.com>
2022-06-01 16:42:02 +0100

    Made changes

--- script.js
+++ script.js
@@ -30,6 +30,22 @@ hamburger.addEventListener("click", () => {
   });
 });
 
+
+link.forEach((e) => {
+  e.addEventListener("click", () => {
+    nav.classList.remove("switch");
+    link.forEach((e) => {
+      e.classList.remove("toggle");
+    });
+    bun.forEach((e) => {
+      e.classList.remove("toggle");
+    });
+    bunCrust.forEach((e) => {
+      e.classList.remove("toggle");
+    });
+  });
+});
+
 //Sticky Header With Intersection Observer
 
 /*
